f62821b86a0caf01eb3c48b4689bc5883152d77e
Kasey <yesakmac@gmail.com>
2014-10-30 11:18:58 -0500

    Needed to add the MongoDB connect info to routes/tasklist.js

--- routes/tasklist.js
+++ routes/tasklist.js
@@ -1,6 +1,15 @@
 /**
  * Created by KJ79607 on 10/30/2014.
  */
+var mongoose = require('mongoose'),
+    task = require('../models/task.js');
+
+module.exports = TaskList;
+
+function TaskList(connection) {
+    mongoose.connect(connection);
+}
+
 TaskList.prototype = {
     showTasks: function(req, res) {
         task.find({itemCompleted: false}, function foundTasks(err, items) {
